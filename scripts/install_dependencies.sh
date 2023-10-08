df4e812dc7f73eb37a86ea5525b3f15f53346b79
Navan Chauhan <navanchauhan@gmail.com>
2023-10-08 10:43:38 -0600

    add service files

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -4,5 +4,10 @@ then
 	curl -sSL https://install.python-poetry.org | python3 -
 fi
 export PATH="/root/.local/bin:$PATH"
-cd /home/ec2-user/dep-dir
-/root/.local/bin/poetry install
+cd /home/ec2-user
+sudo chown -R ec2-user:ec2-user /home/ec2-user/deps-dir
+cd /home/ec2-user/deps-dir
+sudo -u ec2-user bash -c 'cd /home/ec2-user/deps-dir && /home/ec2-user/.local/bin/poetry install'
+
+systemctl daemon-reload
+
