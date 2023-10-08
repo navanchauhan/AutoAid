1be2d73a74eb599d389b94d203ca4a44a9ceff9f
Navan Chauhan <navanchauhan@gmail.com>
2023-10-08 10:32:35 -0600

    trying to add CD

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -4,5 +4,5 @@ then
 	curl -sSL https://install.python-poetry.org | python3 -
 fi
 export PATH="/root/.local/bin:$PATH"
-cd /home/ec2-user
+cd /home/ec2-user/dep-dir
 /root/.local/bin/poetry install
